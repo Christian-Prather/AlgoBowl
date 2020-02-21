2733c143ac0d3941c21a130230770fe52d41a853
Christian <christianprather@smilingrobotics.com>
2020-02-21 08:54:23 -0700

    Edited parameters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import simulated_annealing
 TEMP = 2000
 COOLING = 0.99
 STEP_SIZE = 2
+MIN_TEMP = 5
 
 def main():
     print("AlgoBowl.......")
@@ -9,7 +10,7 @@ def main():
     simulated_annealing.generate_start_state()
    
     # Not sure when to stop?
-    for i in range(5):
+    while TEMP > MIN_TEMP:
         simulated_annealing.choose_next_node(TEMP, STEP_SIZE)
         TEMP *= COOLING
 
