7e98c51b1e46ee2f9bee6501db72c8c445faca32
Christian Prather <Clemsbach@gmail.com>
2020-02-23 13:01:55 -0700

    Added Progress bar(not working

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import simulated_annealing
+from tqdm import tqdm
+
+
 TEMP = 2000
 COOLING = 0.99
 STEP_SIZE = 2
 MIN_TEMP = 5
-
 def main():
     print("AlgoBowl.......")
     global TEMP
-    simulated_annealing.generate_start_state()
-   
-    # Not sure when to stop?
-    while TEMP > MIN_TEMP:
-        simulated_annealing.choose_next_node(TEMP, STEP_SIZE)
-        TEMP *= COOLING
+    with tqdm(total = TEMP) as pbar:
+        simulated_annealing.generate_start_state()
+        # Not sure when to stop?
+        while TEMP > MIN_TEMP:
+            pbar.update()
+            simulated_annealing.choose_next_node(TEMP, STEP_SIZE)
+            TEMP *= COOLING
 
-    print(simulated_annealing.get_currentNode())
+        print(simulated_annealing.get_currentNode())
 if __name__== "__main__":
     main()
